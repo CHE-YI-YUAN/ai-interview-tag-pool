b4075fe957952c064024cbbd2f13ebae7fb1cf4b
CHE-YI-YUAN <harrypotter.mbit@gmail.com>
2026-05-22 23:43:34 +0800

    Initialize package structure

---
+++ src/ai_interview_tag_pool/__init__.py
@@ -0,0 +1,11 @@
+"""AI Interview Tag Pool - Intelligent subtitle tagging system."""
+
+__version__ = "0.1.0"
+__author__ = "CHE-YI-YUAN"
+
+from .config import Config, get_config
+
+__all__ = [
+    "Config",
+    "get_config",
+]
