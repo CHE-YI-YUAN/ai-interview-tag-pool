dfb5520c9a40f7a343140360a63c6544642887f4
CHE-YI-YUAN <harrypotter.mbit@gmail.com>
2026-05-23 10:58:43 +0800

    Add tagging categories and segment size to config.py

---
+++ src/ai_interview_tag_pool/tagging_config.py
@@ -0,0 +1,12 @@
+# Tagging categories for subtitle segment tagging
+TAGGING_POOL = [
+    "economy",
+    "technology",
+    "politics",
+    "sports",
+    "health",
+    "entertainment"
+]
+
+# Number of sentences in each segment
+SEGMENT_SIZE = 3
